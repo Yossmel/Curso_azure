b3a928e6789969138f0e120842632f19f78e003e
Alumno Torre Tecnológica <alu_torre1@instructores.senati.local>
2024-10-25 11:12:59 -0500

    feat: modify passwd!

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:senati-bd.database.windows.net,1433; Database = senati-db", "ybrunoamado", "{BRUNOamado123}");
+    $conn = new PDO("sqlsrv:server = tcp:senati-bd.database.windows.net,1433; Database = senati-db", "ybrunoamado", "BRUNOamado123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ybrunoamado", "pwd" => "{BRUNOamado123}", "Database" => "senati-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "ybrunoamado", "pwd" => "BRUNOamado123", "Database" => "senati-db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:senati-bd.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
